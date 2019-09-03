8256118058547e2ca82da5fa6a33881e48293096
Zavid <zavidegbue@gmail.com>
2019-09-03 19:24:55 +0100

    Fixed cookie expiry

--- constants/cookies.js
+++ constants/cookies.js
@@ -35,7 +35,7 @@ export class CookiePrompt extends Component {
 export function setCookie(name, value, hours) {
   const date = new Date();
   date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
-  const expires = `${expires}${date.toUTCString()}`;
+  const expires = `expires=${date.toUTCString()}`;
   document.cookie = `${name}=${value};${expires};path=/`;
 }
 
