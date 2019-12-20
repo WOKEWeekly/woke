ef5d3ca2de58fe264d0f9a2d33ece7255d4b1c02
Zavid <zavidegbue@gmail.com>
2019-12-20 21:18:36 +0000

    Fully-constructed message

--- src/private/cron.js
+++ src/private/cron.js
@@ -4,7 +4,7 @@ const slack = require('./slack.js');
 module.exports = function(conn){
 
   /** Notify executives of team member birthdays */
-  schedule.scheduleJob({second: 1}, function(){
+  schedule.scheduleJob({ hour: 9 }, function(){
     conn.query("SELECT * FROM team WHERE DATE_FORMAT(birthday,'%m-%d') = DATE_FORMAT(CURDATE(),'%m-%d')", function (err, result) {
       if (err) return console.log(err.toString());
       if (result.length === 0) return console.log("Birthdays: It's no one's birthday today.");
--- src/private/slack.js
+++ src/private/slack.js
@@ -5,20 +5,27 @@ const slack = new WebClient(token);
 
 module.exports = {
   sendBirthdayMessage: async(member) => {
-    const channel = 'test';
-    const message = `It's ${member.firstname}'s birthday today! Happy Birthday to you! <@${member.slackID}>`;
+    const channel = process.env.LOCAL_ENV ? 'test' : 'discussions';
+    let pronoun, title;
+
+    if (member.sex === 'M'){
+      pronoun = 'his';
+      title = 'KING';
+    } else {
+      pronoun = 'her';
+      title = 'QUEEN';
+    }
+
+    let message = `Shout out to our ${member.role}, *${member.firstname} ${member.lastname}*, on ${pronoun} birthday! Happy Birthday to you, ${title}! :tada: :heart:`;
+
+    if (member.slackID !== null){
+      message += ` <@${member.slackID}>`
+    }
+
     await slack.chat.postMessage({
       channel,
       username: '#WOKEWeekly',
-      blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: message
-            }
-          }
-      ]
+      blocks: [{ type: 'section', text: { type: 'mrkdwn', text: message } }]
     });
   }
 }
