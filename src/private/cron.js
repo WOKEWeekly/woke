92fec0bf230706385c8f7d39c8341009de1771f2
Zavid <zavidegbue@gmail.com>
2020-01-03 21:25:01 +0000

    Randomise Slack birthday messages

--- src/private/cron.js
+++ src/private/cron.js
@@ -1,11 +1,17 @@
 const schedule = require('node-schedule');
 const slack = require('./slack.js');
 
+const time = { hour: 9, minute: 0 };
+const sql = "SELECT * FROM team WHERE DATE_FORMAT(birthday,'%m-%d') = DATE_FORMAT(CURDATE(),'%m-%d')";
+
+// const time = { second: 0 };
+// const sql = "SELECT * FROM team LIMIT 1";
+
 module.exports = function(conn){
 
   /** Notify executives of team member birthdays */
-  schedule.scheduleJob({ hour: 10, minute: 15 }, function(){
-    conn.query("SELECT * FROM team WHERE DATE_FORMAT(birthday,'%m-%d') = DATE_FORMAT(CURDATE(),'%m-%d')", function (err, result) {
+  schedule.scheduleJob({ time }, function(){
+    conn.query(sql, function (err, result) {
       if (err) return console.log(err.toString());
       if (!result.length) return console.log("Birthdays: It's no one's birthday today.");
 
--- src/private/slack.js
+++ src/private/slack.js
@@ -5,18 +5,18 @@ const slack = new WebClient(token);
 
 module.exports = {
   sendBirthdayMessage: async(member) => {
-    const channel = process.env.LOCAL_ENV ? 'test' : 'discussions';
+    const channel = process.env.LOCAL_ENV ? 'test' : 'general';
     let pronoun, title;
 
     if (member.sex === 'M'){
       pronoun = 'his';
-      title = 'KING';
+      title = 'king';
     } else {
       pronoun = 'her';
-      title = 'QUEEN';
+      title = 'queen';
     }
 
-    let message = `Shout out to our ${member.role}, *${member.firstname} ${member.lastname}*, on ${pronoun} birthday! Happy Birthday to you, ${title}! :tada: :heart:`;
+    let message = `${randomMessage(intros)} to our ${member.role}, *${member.firstname} ${member.lastname}*, on ${pronoun} birthday! Happy Birthday to you, ${title}! ${randomMessage(outros)}`;
 
     if (member.slackID !== null){
       message += ` <@${member.slackID}>`
@@ -29,3 +29,23 @@ module.exports = {
     });
   }
 }
+
+const randomMessage = (messages) => {
+  return messages[Math.floor(Math.random() * messages.length)];
+}
+
+const intros = [
+  'Shout out',
+  'Big love',
+  'More life',
+  'Best wishes'
+];
+
+const outros = [
+  'Blessings! :heart:',
+  'Keep being great! :muscle::skin-tone-5: :raised_hands::skin-tone-5:',
+  'Stay blessed! :pray::skin-tone-5: :heart:',
+  'Have an amazing day! :tada: :heart:',
+  'Enjoy your day! :zany_face: :dizzy:',
+  'Live it up! :clinking_glasses: :tada:'
+];
