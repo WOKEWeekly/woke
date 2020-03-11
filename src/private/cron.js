30eba6c25233f1e20a3373f3e9cad67de3e61c16
Zavid <zavidegbue@gmail.com>
2020-03-11 19:12:56 +0000

    Remind #general of sessions happening on the day

--- src/private/cron.js
+++ src/private/cron.js
@@ -1,25 +1,37 @@
 const schedule = require('node-schedule');
 const slack = require('./slack.js');
 
-const time = { hour: 7, minute: 0 };
-const sql = "SELECT * FROM team WHERE DATE_FORMAT(birthday,'%m-%d') = DATE_FORMAT(CURDATE(),'%m-%d')";
-
-// const time = { second: 0 };
-// const sql = "SELECT * FROM team LIMIT 1";
+const testInterval = { second: 0 };
+const birthdayReminderTime = { hour: 7, minute: 0 };
+const sessionReminderTime = { hour: 10, minute: 0 };
 
 module.exports = function(conn){
 
-  /** Notify executives of team member birthdays */
-  schedule.scheduleJob(time, function(){
+  /** Notify General Slack channel of team member birthdays at 7:00am */
+  schedule.scheduleJob(birthdayReminderTime, function(){
+    const sql = "SELECT * FROM team WHERE DATE_FORMAT(birthday,'%m-%d') = DATE_FORMAT(CURDATE(),'%m-%d')";
     conn.query(sql, function (err, result) {
       if (err) return console.log(err.toString());
       if (!result.length) return console.log("Birthdays: It's no one's birthday today.");
 
-      for(let member of result) {
+      for (let member of result) {
         slack.sendBirthdayMessage(member);
         console.log(`Birthday message sent for ${member.firstname} ${member.lastname}.`);
       }
+    });
+  });
 
+  /** Notify General Slack channel of sessions occurring on the current day at 10:00am */
+  schedule.scheduleJob(sessionReminderTime, function(){
+    const sql = "SELECT * FROM sessions WHERE dateheld = CURRENT_DATE()";
+    conn.query(sql, function (err, result) {
+      if (err) return console.log(err.toString());
+      if (!result.length) return console.log("Session Reminders: There are no sessions taking place today.");
+
+      for (let session of result) {
+        slack.sendSessionReminder(session);
+        console.log(`Session reminder sent for ${session.title}.`);
+      }
     });
   });
 }
--- src/private/slack.js
+++ src/private/slack.js
@@ -2,50 +2,101 @@ const { WebClient } = require('@slack/web-api');
 const token = process.env.SLACK_TOKEN;
 
 const slack = new WebClient(token);
+const channel = process.env.LOCAL_ENV ? 'UDEMRRR8T' : 'general';
 
 module.exports = {
   sendBirthdayMessage: async(member) => {
-    const channel = process.env.LOCAL_ENV ? 'test' : 'general';
-    let pronoun, title;
-
-    if (member.sex === 'M'){
-      pronoun = 'his';
-      title = 'king';
-    } else {
-      pronoun = 'her';
-      title = 'queen';
-    }
-
-    let message = `${randomMessage(intros)} to our ${member.role}, *${member.firstname} ${member.lastname}*, on ${pronoun} birthday! Happy Birthday to you, ${title}! ${randomMessage(outros)}`;
-
-    if (member.slackID !== null){
-      message += ` <@${member.slackID}>`
-    }
-
-    await slack.chat.postMessage({
-      channel,
-      username: '#WOKEWeekly',
-      blocks: [{ type: 'section', text: { type: 'mrkdwn', text: message } }]
-    });
+    const message = constructBirthdayMessage(member);
+    await postMessage(message);
+  },
+  sendSessionReminder: async(session) => {
+    const message = constructSessionReminderMessage(session);
+    await postMessage(message);
   }
 }
 
-const randomMessage = (messages) => {
+/**
+ * Retrieve a random message from an array of messages;
+ * @param {string[]} messages - An array of messages.
+ * @returns A random message.
+ */
+const getRandomMessage = (messages) => {
   return messages[Math.floor(Math.random() * messages.length)];
 }
 
-const intros = [
-  'Shout out',
-  'Big love',
-  'More life',
-  'Best wishes'
-];
+/**
+ * Post a constructed message to the Slack channel.
+ * @param {string} message - The message to be sent.
+ * @returns The sender function.
+ */
+const postMessage = (message) => {
+  return slack.chat.postMessage({
+    channel,
+    username: '#WOKEWeekly',
+    blocks: [{ type: 'section', text: { type: 'mrkdwn', text: message } }]
+  });
+}
 
-const outros = [
-  'Blessings! :heart:',
-  'Keep being great! :muscle::skin-tone-5: :raised_hands::skin-tone-5:',
-  'Stay blessed! :pray::skin-tone-5: :heart:',
-  'Have an amazing day! :tada: :heart:',
-  'Enjoy your day! :zany_face: :dizzy:',
-  'Live it up! :clinking_glasses: :tada:'
-];
+/**
+ * Constructs the birthday message to be sent.
+ * @param {Object} member - The details of the celebrating member.
+ * @returns The constructed message.
+ */
+const constructBirthdayMessage = (member) => {
+  let pronoun, title;
+
+  if (member.sex === 'M'){
+    pronoun = 'his';
+    title = 'king';
+  } else {
+    pronoun = 'her';
+    title = 'queen';
+  }
+
+  const intros = [
+    'Shout out',
+    'Big love',
+    'More life',
+    'Best wishes'
+  ];
+  
+  const outros = [
+    'Blessings! :heart:',
+    'Keep being great! :muscle::skin-tone-5: :raised_hands::skin-tone-5:',
+    'Stay blessed! :pray::skin-tone-5: :heart:',
+    'Have an amazing day! :tada: :heart:',
+    'Enjoy your day! :zany_face: :dizzy:',
+    'Live it up! :clinking_glasses: :tada:'
+  ];
+
+  let message = `${getRandomMessage(intros)} to our ${member.role}, *${member.firstname} ${member.lastname}*, on ${pronoun} birthday! Happy Birthday to you, ${title}! ${getRandomMessage(outros)}`;
+
+  if (member.slackID !== null){
+    message += ` <@${member.slackID}>`
+  }
+
+  return message;
+}
+
+/**
+ * Constructs the session reminder message to be sent.
+ * @param {string} session.title - The title of the session.
+ * @returns The constructed message.
+ */
+const constructSessionReminderMessage = ({title}) => {
+  const firstSentences = [
+    `We have the *${title}* session taking place today!`,
+    `We got *${title}* on today!`,
+    `Another day, another sesh: *${title}*.`
+  ];
+
+  const secondSentences = [
+    'Good luck to the team hosting it! :raised_hands::skin-tone-5:',
+    'Do your best, team! :heart:',
+    'Represent, team! :call_me_hand::skin-tone-5:',
+    'Wish the team luck! :star:',
+    'Let\'s do it! :muscle::skin-tone-5:'
+  ];
+
+  return `${getRandomMessage(firstSentences)} ${getRandomMessage(secondSentences)}`;
+}
