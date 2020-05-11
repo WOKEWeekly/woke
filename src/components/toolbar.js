b220d4ba04fb400adca6761c4992beb5f51608bc
Zavid <zavidegbue@gmail.com>
2020-05-11 21:25:57 +0100

    Correct toolbar bug

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -28,12 +28,12 @@ class Toolbar extends Component {
 
 export class TopToolbar extends Component {
   render() {
-    return <Toolbar className={css.top_toolbar} />;
+    return <Toolbar className={css.top_toolbar} {...this.props} />;
   }
 }
 
 export class BottomToolbar extends Component {
   render() {
-    return <Toolbar className={css.bottom_toolbar} />;
+    return <Toolbar className={css.bottom_toolbar} {...this.props} />;
   }
 }
