c417ba8e895c521e2c0695959598c8dd20a88dde
Zavid <zavidegbue@gmail.com>
2020-05-23 10:55:56 +0100

    Prevent re-render of date field

--- src/pages/articles/form.js
+++ src/pages/articles/form.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
-import { SubmitButton, CancelButton } from '~/components/button.js';
-import { AuthoredDatePicker } from '~/components/datepicker.js';
+import { SubmitButton, CancelButton } from 'components/button.js';
+import { AuthoredDatePicker } from 'components/datepicker.js';
 import {
   Heading,
   Group,
@@ -14,200 +14,204 @@ import {
   LongTextArea,
   FileSelector,
   Select
-} from '~/components/form.js';
-import { Shader, Spacer } from '~/components/layout.js';
+} from 'components/form.js';
+import { Shader, Spacer } from 'components/layout.js';
 
-import { categories } from '~/constants/categories.js';
-import CLEARANCES from '~/constants/clearances.js';
-import request from '~/constants/request.js';
-import { ARTICLE_STATUS } from '~/constants/strings.js';
+import { categories } from 'constants/categories.js';
+import CLEARANCES from 'constants/clearances.js';
+import request from 'constants/request.js';
+import { ARTICLE_STATUS } from 'constants/strings.js';
 
-import css from '~/styles/pages/Articles.module.scss';
+import css from 'styles/pages/Articles.module.scss';
 
-class ArticleForm extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      authors: []
-    };
-
-    if (props.user.clearance < CLEARANCES.ACTIONS.CRUD_ARTICLES) {
-      return (location.href = '/');
-    }
+const ArticleForm = ({
+  article,
+  heading,
+  confirmText,
+  confirmFunc,
+  cancelFunc,
+  handlers,
+  operation,
+  user
+}) => {
+  if (user.clearance < CLEARANCES.ACTIONS.CRUD_ARTICLES) {
+    return (location.href = '/');
   }
 
-  componentDidMount() {
+  const [isLoaded, setLoaded] = useState(false);
+  const [authors, setAuthors] = useState([]);
+  const [isDateFieldVisible, setVisibility] = useState(false);
+
+  const { handleText, handleDate, handleFile } = handlers;
+  const {
+    title,
+    content,
+    category,
+    excerpt,
+    image,
+    authorId,
+    status,
+    datePublished,
+    tags
+  } = article;
+
+  useEffect(() => {
     request({
       url: '/api/v1/members/authors',
       method: 'GET',
       headers: { Authorization: process.env.AUTH_KEY },
       onSuccess: (response) => {
-        const authors = [];
-        response.forEach((author) => {
-          authors.push({
-            value: author.id,
-            label: `${author.firstname} ${author.lastname}`
+        const authors = response
+          .map((author) => {
+            return {
+              value: author.id,
+              label: `${author.firstname} ${author.lastname}`
+            };
+          })
+          .sort((a, b) => {
+            a = a.label;
+            b = b.label;
+            return a < b ? -1 : a > b ? 1 : 0;
           });
-        });
-        authors.sort((a, b) => {
-          a = a.label;
-          b = b.label;
-          return a < b ? -1 : a > b ? 1 : 0;
-        });
-        this.setState({ authors });
+        setAuthors(authors);
+        setLoaded(true);
       }
     });
-  }
+  }, [isLoaded]);
 
-  render() {
-    const {
-      heading,
-      confirmText,
-      confirmFunc,
-      cancelFunc,
-      handlers,
-      operation
-    } = this.props;
-    const { handleText, handleDate, handleFile } = handlers;
-    const {
-      title,
-      content,
-      category,
-      excerpt,
-      image,
-      authorId,
-      status,
-      datePublished,
-      tags
-    } = this.props.article;
+  useEffect(() => {
+    setVisibility(status === ARTICLE_STATUS.PUBLISHED);
+  });
 
-    const DatePublished = () => {
-      if (status !== ARTICLE_STATUS.PUBLISHED) return null;
+  return (
+    <Shader>
+      <Spacer className={css.form}>
+        <div>
+          <Heading>{heading}</Heading>
 
-      return (
-        <Group>
-          <Col>
-            <Label>Date Published:</Label>
-            <AuthoredDatePicker
-              name={'datePublished'}
-              date={datePublished}
-              onConfirm={handleDate}
-            />
-          </Col>
-        </Group>
-      );
-    };
+          <Group>
+            <Col md={7}>
+              <Label>Title:</Label>
+              <TextInput
+                name={'title'}
+                value={title}
+                onChange={handleText}
+                placeholder={'Enter the title.'}
+              />
+            </Col>
+            <Col md={5}>
+              <Label>Category:</Label>
+              <Select
+                name={'category'}
+                value={category}
+                placeholder={'Select a category.'}
+                items={categories}
+                onChange={handleText}
+              />
+            </Col>
+          </Group>
+          <Group>
+            <Col>
+              <LabelInfo>Content:</LabelInfo>
+              <LongTextArea
+                name={'content'}
+                value={content}
+                onChange={handleText}
+                placeholder={'Write your thoughts. Express yourself.'}
+              />
+            </Col>
+          </Group>
+          <Group>
+            <Col>
+              <Label>Excerpt:</Label>
+              <ShortTextArea
+                name={'excerpt'}
+                value={excerpt}
+                onChange={handleText}
+                placeholder={"Enter this article's excerpt."}
+              />
+            </Col>
+          </Group>
+          <Group>
+            <Col md={6}>
+              <Label>Author:</Label>
+              <Select
+                name={'authorId'}
+                value={authorId}
+                placeholder={'Select the author.'}
+                items={authors}
+                onChange={handleText}
+              />
+            </Col>
+            <Col md={{ span: 4, offset: 2 }}>
+              <Label>Status:</Label>
+              <Select
+                name={'status'}
+                value={status}
+                placeholder={'Select a status.'}
+                items={Object.keys(ARTICLE_STATUS).map((key) => key)}
+                onChange={handleText}
+              />
+            </Col>
+          </Group>
+          <DatePublished
+            isDateFieldVisible={isDateFieldVisible}
+            datePublished={datePublished}
+            handleDate={handleDate}
+          />
+          <Group>
+            <Col>
+              <FileSelector
+                image={image}
+                operation={operation}
+                onChange={handleFile}
+              />
+            </Col>
+          </Group>
+          <Group>
+            <Col>
+              <Label>Tags:</Label>
+              <ShortTextArea
+                name={'tags'}
+                value={tags}
+                onChange={handleText}
+                placeholder={'e.g. woke, society, black women'}
+              />
+            </Col>
+          </Group>
+        </div>
 
-    return (
-      <Shader>
-        <Spacer className={css.form}>
-          <div>
-            <Heading>{heading}</Heading>
+        <div>
+          <Group>
+            <Col>
+              <SubmitButton onClick={confirmFunc} className={'mr-2'}>
+                {confirmText}
+              </SubmitButton>
+              <CancelButton onClick={cancelFunc}>Cancel</CancelButton>
+            </Col>
+          </Group>
+        </div>
+      </Spacer>
+    </Shader>
+  );
+};
 
-            <Group>
-              <Col md={7}>
-                <Label>Title:</Label>
-                <TextInput
-                  name={'title'}
-                  value={title}
-                  onChange={handleText}
-                  placeholder={'Enter the title.'}
-                />
-              </Col>
-              <Col md={5}>
-                <Label>Category:</Label>
-                <Select
-                  name={'category'}
-                  value={category}
-                  placeholder={'Select a category.'}
-                  items={categories}
-                  onChange={handleText}
-                />
-              </Col>
-            </Group>
-            <Group>
-              <Col>
-                <LabelInfo>Content:</LabelInfo>
-                <LongTextArea
-                  name={'content'}
-                  value={content}
-                  onChange={handleText}
-                  placeholder={'Write your thoughts. Express yourself.'}
-                />
-              </Col>
-            </Group>
-            <Group>
-              <Col>
-                <Label>Excerpt:</Label>
-                <ShortTextArea
-                  name={'excerpt'}
-                  value={excerpt}
-                  onChange={handleText}
-                  placeholder={"Enter this article's excerpt."}
-                />
-              </Col>
-            </Group>
-            <Group>
-              <Col md={6}>
-                <Label>Author:</Label>
-                <Select
-                  name={'authorId'}
-                  value={authorId}
-                  placeholder={'Select the author.'}
-                  items={this.state.authors}
-                  onChange={handleText}
-                />
-              </Col>
-              <Col md={{ span: 4, offset: 2 }}>
-                <Label>Status:</Label>
-                <Select
-                  name={'status'}
-                  value={status}
-                  placeholder={'Select a status.'}
-                  items={Object.keys(ARTICLE_STATUS).map((key) => key)}
-                  onChange={handleText}
-                />
-              </Col>
-            </Group>
-            <DatePublished />
-            <Group>
-              <Col>
-                <FileSelector
-                  image={image}
-                  operation={operation}
-                  onChange={handleFile}
-                />
-              </Col>
-            </Group>
-            <Group>
-              <Col>
-                <Label>Tags:</Label>
-                <ShortTextArea
-                  name={'tags'}
-                  value={tags}
-                  onChange={handleText}
-                  placeholder={'e.g. woke, society, black women'}
-                />
-              </Col>
-            </Group>
-          </div>
+const DatePublished = ({ isDateFieldVisible, datePublished, handleDate }) => {
+  if (!isDateFieldVisible) return null;
 
-          <div>
-            <Group>
-              <Col>
-                <SubmitButton onClick={confirmFunc} className={'mr-2'}>
-                  {confirmText}
-                </SubmitButton>
-                <CancelButton onClick={cancelFunc}>Cancel</CancelButton>
-              </Col>
-            </Group>
-          </div>
-        </Spacer>
-      </Shader>
-    );
-  }
-}
+  return (
+    <Group>
+      <Col>
+        <Label>Date Published:</Label>
+        <AuthoredDatePicker
+          name={'datePublished'}
+          date={datePublished}
+          onConfirm={handleDate}
+        />
+      </Col>
+    </Group>
+  );
+};
 
 const mapStateToProps = (state) => ({
   user: state.user
