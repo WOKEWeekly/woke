1be11be5a4c59300974588bd05cda22190977a83
David Egbue <david.egbue@ibm.com>
2020-07-02 11:23:00 +0100

    Separate and add comments

--- src/private/cron/index.js
+++ src/private/cron/index.js
@@ -1,15 +1,9 @@
-const async = require('async');
-const fetch = require('node-fetch');
 const schedule = require('node-schedule');
 
-const slack = require('./slack.js');
-
-const knex = require('../singleton/knex').getKnex();
+const { notifyMemberBirthday, notifySessionToday } = require('./database');
+const { notifyDueExecTasks, notifyUncaptionedCards } = require('./trello');
 
 const isDev = process.env.NODE_ENV !== 'production';
-const TRELLO_API_KEY = process.env.TRELLO_API_KEY;
-const TRELLO_TOKEN = process.env.TRELLO_TOKEN;
-const queryParams = `key=${TRELLO_API_KEY}&token=${TRELLO_TOKEN}`;
 
 /** 5 second interval for test messages. */
 const testInterval = '*/5 * * * * *';
@@ -19,13 +13,6 @@ const isBirthdayTest = isDev && false;
 const isSessionTest = isDev && false;
 const isDueExecTaskTest = isDev && true;
 const isPostsWithoutCaptionTest = isDev && false;
-
-/** The Trello board IDs */
-const BOARDS = {
-  EXEC: 'cPCwhUI4',
-  GENERAL: 'mbzVPEE6',
-  SOCIAL_MEDIA: 'qKuY4vcl'
-};
 
 /** The intervals for each cron job */
 const INTERVALS = {
@@ -62,130 +49,4 @@ module.exports = () => {
     isPostsWithoutCaptionTest ? testInterval : INTERVALS.POSTS_WITHOUT_CAPTIONS,
     notifyUncaptionedCards
   );
-};
-
-/** Notify General Slack channel of team member birthdays */
-const notifyMemberBirthday = () => {
-  const query = knex
-    .select()
-    .from('members')
-    .whereRaw("DATE_FORMAT(birthday,'%m-%d') = DATE_FORMAT(CURDATE(),'%m-%d')");
-  query.asCallback(function (err, result) {
-    if (err) return console.error(err.toString());
-    if (!result.length)
-      return console.info("Birthdays: It's no one's birthday today.");
-
-    for (let member of result) {
-      slack.sendBirthdayMessage(member);
-      console.info(
-        `Birthday message sent for ${member.firstname} ${member.lastname}.`
-      );
-    }
-  });
-};
-
-/** Notify General Slack channel of sessions occurring on the current day */
-const notifySessionToday = () => {
-  const query = knex
-    .select()
-    .from('sessions')
-    .whereRaw('dateHeld = CURRENT_DATE()');
-  query.asCallback(function (err, result) {
-    if (err) return console.error(err.toString());
-    if (!result.length)
-      return console.info(
-        'Session Reminders: There are no sessions taking place today.'
-      );
-
-    for (let session of result) {
-      slack.sendSessionReminder(session);
-      console.info(`Session reminder sent for ${session.title}.`);
-    }
-  });
-};
-
-const notifyDueExecTasks = () => {
-  getCardsFromTrelloBoard(BOARDS.EXEC, (cards) => {
-    const seenMembers = {};
-    const execTasks = cards
-      .map((card) => {
-        const { name, due, dueComplete, idMembers } = card;
-        const dueInMs = new Date(due).getTime();
-
-        // If card has a due date, was due in the past
-        // and has not been completed...
-        if (due && dueInMs < Date.now() && !dueComplete) {
-          const members = idMembers.map((memberId) => {
-            seenMembers[memberId] = '';
-            return memberId;
-          });
-
-          return { name, due, members };
-        }
-      })
-      .filter((e) => e);
-
-    if (!execTasks.length) return;
-
-    async.mapValues(
-      seenMembers,
-      function (value, id, callback) {
-        getMemberByTrelloId(id, (name) => {
-          const firstname = name.split(' ')[0];
-          callback(null, firstname);
-        });
-      },
-      function (err, memberMapping) {
-        const execTasksWithMembers = execTasks.map((card) => {
-          card.members = card.members.map((member) => {
-            return memberMapping[member];
-          });
-
-          return card;
-        });
-
-        slack.sendDueExecTasks(execTasksWithMembers);
-      }
-    );
-  });
-};
-
-/** Notify Social Media Slack channel of uncaptioned cards */
-const notifyUncaptionedCards = () => {
-  getCardsFromTrelloBoard(BOARDS.SOCIAL_MEDIA, (cards) => {
-    const postWithoutCaptions = cards
-      .map((card) => {
-        const { name, desc, due } = card;
-        const dueinMs = new Date(due).getTime();
-
-        // If card has a due date, is due in the future
-        // and doesn't already have a caption...
-        if (due && dueinMs > Date.now() && !desc) {
-          return { name, due };
-        }
-      })
-      .filter((e) => e);
-
-    if (postWithoutCaptions.length) {
-      slack.sendPostsWithoutCaptions(postWithoutCaptions);
-    }
-  });
-};
-
-const getCardsFromTrelloBoard = (boardId, callback) => {
-  fetch(`https://api.trello.com/1/boards/${boardId}/cards?${queryParams}`, {
-    method: 'GET'
-  })
-    .then((res) => res.json())
-    .then((cards) => callback(cards))
-    .catch((err) => console.error(err));
-};
-
-const getMemberByTrelloId = (memberId, callback) => {
-  fetch(`https://api.trello.com/1/members/${memberId}?${queryParams}`, {
-    method: 'GET'
-  })
-    .then((res) => res.json())
-    .then(({ fullName }) => callback(fullName))
-    .catch((err) => console.error(err));
 };
---
+++ src/private/cron/trello.js
@@ -0,0 +1,118 @@
+const async = require('async');
+const fetch = require('node-fetch');
+
+const slack = require('./slack.js');
+
+const TRELLO_API_KEY = process.env.TRELLO_API_KEY;
+const TRELLO_TOKEN = process.env.TRELLO_TOKEN;
+const authorization = `key=${TRELLO_API_KEY}&token=${TRELLO_TOKEN}`;
+
+/** Map of Trello board IDs */
+const BOARDS = {
+  EXEC: 'cPCwhUI4',
+  GENERAL: 'mbzVPEE6',
+  SOCIAL_MEDIA: 'qKuY4vcl'
+};
+
+/** Notify Exec Slack channel of due tasks */
+exports.notifyDueExecTasks = () => {
+  getCardsFromTrelloBoard(BOARDS.EXEC, (cards) => {
+    const seenMembers = {};
+
+    // Retrieve all due cards on the executive board.
+    const execTasks = cards
+      .map((card) => {
+        const { name, due, dueComplete, idMembers } = card;
+        const dueInMs = new Date(due).getTime();
+
+        // If card has a due date, was due in the past
+        // and has not been completed...
+        if (due && dueInMs < Date.now() && !dueComplete) {
+          const members = idMembers.map((memberId) => {
+            // Add placeholder for each unique member.
+            seenMembers[memberId] = '';
+            return memberId;
+          });
+
+          return { name, due, members };
+        }
+      })
+      .filter((e) => e);
+
+    // If there are no due exec tasks, abort.
+    if (!execTasks.length) return;
+
+    async.mapValues(
+      seenMembers,
+      function (value, id, callback) {
+        // Retrieve the name of each unique member.
+        getMemberByTrelloId(id, (name) => {
+          const firstname = name.split(' ')[0];
+          callback(null, firstname);
+        });
+      },
+      function (err, memberMapping) {
+        // Replace the member IDs on each card with the member names.
+        const execTasksWithMembers = execTasks.map((card) => {
+          card.members = card.members.map((member) => {
+            return memberMapping[member];
+          });
+
+          return card;
+        });
+
+        slack.sendDueExecTasks(execTasksWithMembers);
+      }
+    );
+  });
+};
+
+/** Notify Social Media Slack channel of uncaptioned cards */
+exports.notifyUncaptionedCards = () => {
+  getCardsFromTrelloBoard(BOARDS.SOCIAL_MEDIA, (cards) => {
+    const postWithoutCaptions = cards
+      .map((card) => {
+        const { name, desc, due } = card;
+        const dueinMs = new Date(due).getTime();
+
+        // If card has a due date, is due in the future
+        // and doesn't already have a caption...
+        if (due && dueinMs > Date.now() && !desc) {
+          return { name, due };
+        }
+      })
+      .filter((e) => e);
+
+    if (postWithoutCaptions.length) {
+      slack.sendPostsWithoutCaptions(postWithoutCaptions);
+    }
+  });
+};
+
+/**
+ * Retrieve all cards from a specified Trello board.
+ * @param {string} boardId - The ID of the Trello board.
+ * @param {Function} callback - The function to be performed on the resulting cards.
+ */
+const getCardsFromTrelloBoard = (boardId, callback) => {
+  fetch(`https://api.trello.com/1/boards/${boardId}/cards?${authorization}`, {
+    method: 'GET'
+  })
+    .then((res) => res.json())
+    .then((cards) => callback(cards))
+    .catch((err) => console.error(err));
+};
+
+/**
+ * Get the member corresponding to a specified ID.
+ * @param {string} memberId - The member's ID.
+ * @param {Function} callback - The function to be performed on the resulting member.
+ */
+const getMemberByTrelloId = (memberId, callback) => {
+  fetch(`https://api.trello.com/1/members/${memberId}?${authorization}`, {
+    method: 'GET'
+  })
+    .then((res) => res.json())
+    .then(({ fullName }) => callback(fullName))
+    .catch((err) => console.error(err));
+};
