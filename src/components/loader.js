13468579f751b70d77e0b190c91cf5c5c6c5317c
Zavid <zavidegbue@gmail.com>
2020-07-17 16:08:12 +0100

    Lazy load articles

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,9 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Spinner } from 'react-bootstrap';
+import LazyLoad from 'react-lazyload';
+import VisibilitySensor from 'react-visibility-sensor';
 
 import css from 'styles/components/Layout.module.scss';
 
-export class Loader extends Component {
+/**
+ * The component wrapper used to lazy load elements.
+ * @param {object} props - The component props.
+ * @param {any} props.children - The element to be lazy loaded.
+ * @param {Function} props.setInView - The hook to set whether the child
+ * element is in the viewport.
+ * @param {number} [props.height] - The height of the placeholder. Defaults to 400px.
+ * @param {number} [props.offset] - The distance from the edge of the viewport
+ * before the element is loaded in. Defaults to -100.
+ * @returns {React.Component} The lazy loader component.
+ */
+export const LazyLoader = ({
+  children,
+  setInView,
+  height = 400,
+  offset = -100,
+}) => {
+  const [shouldDetectChange, setDetectivity] = useState(true);
+
+  const toggleVisibility = () => {
+    setInView(true);
+    setDetectivity(false);
+  };
+
+  return (
+    <LazyLoad height={height} offset={offset} once>
+      <VisibilitySensor
+        onChange={toggleVisibility}
+        partialVisibility={true}
+        active={shouldDetectChange}>
+        {children}
+      </VisibilitySensor>
+    </LazyLoad>
+  );
+};
+
+export class Loader extends React.Component {
   render() {
     return (
       <div className={css.loader}>
@@ -14,7 +52,7 @@ export class Loader extends Component {
   }
 }
 
-export class LoaderIcon extends Component {
+export class LoaderIcon extends React.Component {
   render() {
     return (
       <div className={css.loader}>
@@ -24,7 +62,7 @@ export class LoaderIcon extends Component {
   }
 }
 
-export class Empty extends Component {
+export class Empty extends React.Component {
   render() {
     return (
       <div className={css.loader}>
--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -1,11 +1,19 @@
-import React, { Component, memo, useState, useEffect } from 'react';
+import React, { memo, useState, useEffect, useRef } from 'react';
+import LazyLoad from 'react-lazyload';
 import { connect } from 'react-redux';
+import VisibilitySensor from 'react-visibility-sensor';
 import { zDate } from 'zavid-modules';
 
 import { AdminButton } from 'components/button.js';
 import { CloudinaryImage } from 'components/image';
-import { Shader, Spacer, Default, Mobile } from 'components/layout.js';
-import { Loader, Empty } from 'components/loader.js';
+import {
+  Shader,
+  Spacer,
+  Default,
+  Mobile,
+  isSmallDevice
+} from 'components/layout.js';
+import { LazyLoader, Loader, Empty } from 'components/loader.js';
 import { Title, Subtitle, Paragraph, VanillaLink } from 'components/text.js';
 import { BottomToolbar } from 'components/toolbar.js';
 import { Zoomer, Fader } from 'components/transitioner.js';
@@ -13,177 +21,165 @@ import CLEARANCES from 'constants/clearances.js';
 import request from 'constants/request.js';
 import css from 'styles/pages/Articles.module.scss';
 
-class Blog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      isLoaded: false
-    };
-  }
+const Blog = ({ user }) => {
+  const [articles, setArticles] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
 
-  /** Get published articles on mount */
-  componentDidMount() {
-    this.getPublishedArticles();
-  }
+  useEffect(() => {
+    getPublishedArticles();
+  }, [isLoaded]);
 
   /** Get published articles */
-  getPublishedArticles = () => {
+  const getPublishedArticles = () => {
     request({
       url: '/api/v1/articles/published?order=DESC',
       method: 'GET',
       headers: { Authorization: process.env.AUTH_KEY },
       onSuccess: (articles) => {
-        this.setState({
-          articles: articles,
-          isLoaded: true
-        });
+        setArticles(articles);
+        setLoaded(true);
       }
     });
   };
 
-  render() {
-    const { isLoaded, articles } = this.state;
-    const { user } = this.props;
+  const ArticleCollection = () => {
+    if (!isLoaded) {
+      return <Loader />;
+    } else if (articles.length === 0) {
+      return <Empty message={'No articles found.'} />;
+    }
 
-    const ArticleCollection = () => {
-      if (!isLoaded) {
-        return <Loader />;
-      } else if (articles.length === 0) {
-        return <Empty message={'No articles found.'} />;
-      } else {
-        const items = articles.map((article, index) => {
-          return <Article key={index} idx={index} article={article} />;
-        });
+    const items = articles.map((article, key) => {
+      return <Article key={key} idx={key} article={article} />;
+    });
 
-        return <div className={css['article-grid']}>{items}</div>;
-      }
-    };
-
-    return (
-      <Shader>
-        <Spacer gridrows={'auto 1fr auto'}>
-          <Fader determinant={isLoaded} duration={1500}>
-            <ArticleCollection />
-          </Fader>
-        </Spacer>
-
-        <BottomToolbar>
-          {user.clearance >= CLEARANCES.ACTIONS.ARTICLES.MODIFY ? (
-            <AdminButton
-              title={'Blog Admin'}
-              onClick={() => (location.href = '/admin/articles')}
-            />
-          ) : null}
-        </BottomToolbar>
-      </Shader>
-    );
-  }
-}
-
-const Article = memo(({ article, idx }) => {
-  const [isLoaded, setLoaded] = useState(false);
-  useEffect(() => {
-    setLoaded(true);
-  }, [isLoaded]);
-
-  const hyperlink = `/blog/${article.slug}`;
+    return <div className={css['article-grid']}>{items}</div>;
+  };
 
   return (
-    <Zoomer
-      determinant={isLoaded}
-      duration={400}
-      delay={75 * idx}
-      className={css['article-cell']}
-      postTransitions={'background-color .3s ease'}>
-      <Default>
-        <VanillaLink href={hyperlink}>
-          <div className={css['article-cell-contents']}>
-            <CloudinaryImage
-              src={article.coverImage}
-              alt={article.title}
-              className={css['article-image']}
-              lazy={'mw'}
-            />
-            <div className={css['article-details']}>
-              <div className={css['author-image']}>
-                <CloudinaryImage
-                  src={article.authorImage}
-                  title={article.authorName}
-                  lazy={'ss'}
-                />
+    <Shader>
+      <Spacer gridrows={'auto 1fr auto'}>
+        <Fader determinant={isLoaded} duration={1500}>
+          <ArticleCollection />
+        </Fader>
+      </Spacer>
+
+      <BottomToolbar>
+        {user.clearance >= CLEARANCES.ACTIONS.ARTICLES.MODIFY ? (
+          <AdminButton
+            title={'Blog Admin'}
+            onClick={() => (location.href = '/admin/articles')}
+          />
+        ) : null}
+      </BottomToolbar>
+    </Shader>
+  );
+};
+
+const Article = memo(({ article, idx }) => {
+  const hyperlink = `/blog/${article.slug}`;
+
+  const [isInView, setInView] = useState(false);
+
+  return (
+    <LazyLoader setInView={setInView}>
+      <Zoomer
+        determinant={isInView}
+        duration={400}
+        delay={isSmallDevice() ? 75 : 75 * idx}
+        className={css['article-cell']}
+        postTransitions={'background-color .3s ease'}>
+        <Default>
+          <VanillaLink href={hyperlink}>
+            <div className={css['article-cell-contents']}>
+              <CloudinaryImage
+                src={article.coverImage}
+                alt={article.title}
+                className={css['article-image']}
+                lazy={'mw'}
+              />
+              <div className={css['article-details']}>
+                <div className={css['author-image']}>
+                  <CloudinaryImage
+                    src={article.authorImage}
+                    title={article.authorName}
+                    lazy={'ss'}
+                  />
+                </div>
+                <div>
+                  <Title className={css['article-title']}>
+                    {article.title}
+                  </Title>
+                  <Subtitle className={css['article-metadata']}>
+                    Written by {article.authorName}
+                  </Subtitle>
+                  <Subtitle className={css['article-metadata']}>
+                    {article.category} •{' '}
+                    {zDate.formatDate(article.datePublished, true)}
+                  </Subtitle>
+                </div>
               </div>
+              <Paragraph
+                className={css['article-paragraph']}
+                truncate={35}
+                morelink={hyperlink}
+                moretext={'Read the full article'}
+                moreclass={css['article-readmore']}
+                cssOverrides={{
+                  paragraph: css['article-content']
+                }}>
+                {article.content}
+              </Paragraph>
+            </div>
+          </VanillaLink>
+        </Default>
+        <Mobile>
+          <div className={css['article-cell-contents']}>
+            <Title className={css['article-title']}>{article.title}</Title>
+            <VanillaLink href={hyperlink}>
+              <CloudinaryImage
+                src={article.coverImage}
+                alt={article.title}
+                className={css['article-image']}
+                lazy={'mw'}
+              />
+            </VanillaLink>
+            <div className={css['article-metadata-md']}>
+              <CloudinaryImage
+                src={article.authorImage}
+                title={article.authorName}
+                className={css['author-image-md']}
+                lazy={'ss'}
+              />
               <div>
-                <Title className={css['article-title']}>{article.title}</Title>
-                <Subtitle className={css['article-metadata']}>
-                  Written by {article.authorName}
+                <Subtitle>
+                  {article.authorName} • {article.category}
                 </Subtitle>
-                <Subtitle className={css['article-metadata']}>
-                  {article.category} •{' '}
+                <Subtitle>
                   {zDate.formatDate(article.datePublished, true)}
                 </Subtitle>
               </div>
             </div>
             <Paragraph
               className={css['article-paragraph']}
-              truncate={35}
+              truncate={30}
               morelink={hyperlink}
               moretext={'Read the full article'}
               moreclass={css['article-readmore']}
               cssOverrides={{
-                paragraph: css['article-content']
+                paragraph: css['article-content-sm']
               }}>
               {article.content}
             </Paragraph>
           </div>
-        </VanillaLink>
-      </Default>
-      <Mobile>
-        <div className={css['article-cell-contents']}>
-          <Title className={css['article-title']}>{article.title}</Title>
-          <VanillaLink href={hyperlink}>
-            <CloudinaryImage
-              src={article.coverImage}
-              alt={article.title}
-              className={css['article-image']}
-              lazy={'mw'}
-            />
-          </VanillaLink>
-          <div className={css['article-metadata-md']}>
-            <CloudinaryImage
-              src={article.authorImage}
-              title={article.authorName}
-              className={css['author-image-md']}
-              lazy={'ss'}
-            />
-            <div>
-              <Subtitle>
-                {article.authorName} • {article.category}
-              </Subtitle>
-              <Subtitle>
-                {zDate.formatDate(article.datePublished, true)}
-              </Subtitle>
-            </div>
-          </div>
-          <Paragraph
-            className={css['article-paragraph']}
-            truncate={30}
-            morelink={hyperlink}
-            moretext={'Read the full article'}
-            moreclass={css['article-readmore']}
-            cssOverrides={{
-              paragraph: css['article-content-sm']
-            }}>
-            {article.content}
-          </Paragraph>
-        </div>
-      </Mobile>
-    </Zoomer>
+        </Mobile>
+      </Zoomer>
+    </LazyLoader>
   );
 });
 
-const mapStateToProps = (state) => ({
-  user: state.user
+const mapStateToProps = ({ user }) => ({
+  user
 });
 
 export default connect(mapStateToProps)(Blog);
