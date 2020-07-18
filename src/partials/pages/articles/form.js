52942292516b1231104116b3715ce9f596aae967
Zavid <zavidegbue@gmail.com>
2020-07-18 22:55:23 +0100

    Show preview of paragraphs while editing

--- src/partials/pages/articles/form.js
+++ src/partials/pages/articles/form.js
@@ -10,8 +10,6 @@ import {
   Label,
   LabelInfo,
   TextInput,
-  ShortTextArea,
-  LongTextArea,
   Select
 } from 'components/form';
 import {
@@ -19,8 +17,11 @@ import {
   ASPECT_RATIO,
   SELECTOR_LOOK
 } from 'components/form/fileselector';
-import { Shader, Spacer } from 'components/layout.js';
+import { ShortTextArea, LongTextArea } from 'components/form/v2/textarea';
+import { Shader } from 'components/layout.js';
 import { ConfirmModal } from 'components/modal.js';
+import { Paragraph } from 'components/text.js';
+import { Fader } from 'components/transitioner.js';
 import { categories } from 'constants/categories.js';
 import CLEARANCES from 'constants/clearances.js';
 import request from 'constants/request.js';
@@ -29,24 +30,77 @@ import css from 'styles/pages/Articles.module.scss';
 
 import { FILLER_IMAGE_LIMIT } from './helpers';
 
-const ArticleForm = ({
-  article,
-  heading,
-  confirmText,
-  confirmFunc,
-  cancelFunc,
-  handlers,
-  operation,
-  isPublish,
-  compileFillerImages,
-  removeFillerImage,
-  setImagesChanged,
-  user
-}) => {
+/**
+ * The page for the article form and editor preview.
+ * @param {object} props - The component props.
+ * @param {object} props.article - The article object.
+ * @param {object} props.user - The current user.
+ * @returns {React.Component} The component.
+ */
+const ArticleForm = (props) => {
+  const { article, user } = props;
+
   if (user.clearance < CLEARANCES.ACTIONS.ARTICLES.MODIFY) {
     return (location.href = '/');
   }
 
+  const [isLoaded, setLoaded] = useState(false);
+  const [previewVisible, setPreviewVisibility] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, [isLoaded]);
+
+  return (
+    <Shader>
+      <Fader
+        determinant={isLoaded}
+        duration={500}
+        className={previewVisible ? css['article-editor'] : undefined}>
+        <ArticleEditorForm
+          {...props}
+          previewVisible={previewVisible}
+          setPreviewVisibility={setPreviewVisibility}
+        />
+        <ArticleEditorPreview visible={previewVisible} text={article.content} />
+      </Fader>
+    </Shader>
+  );
+};
+
+/**
+ * The form of the article editor.
+ * @param {object} props - The component props.
+ * @param {object} props.article - The article object.
+ * @param {Function} props.cancelFunc - The function called on clicking 'Cancel'.
+ * @param {Function} props.compileFillerImages - The hook for compiling article filler images.
+ * @param {Function} props.confirmFunc - The function called on clicking 'Confirm'.
+ * @param {string} props.confirmText - The text shown on the confirmation button.
+ * @param {object} props.handlers - The hooks for handling form input.
+ * @param {string} props.heading - The form heading.
+ * @param {boolean} props.isPublish - Indicates whether the operation is a publish.
+ * @param {string} props.operation - Either a CREATE or an UPDATE.
+ * @param {boolean} props.previewVisible - Hook state indicating if editor preview is visible.
+ * @param {Function} props.removeFillerImage - The function for removing a filler image from selection.
+ * @param {Function} props.setImagesChanged - The hook for setting whether images have changed in the form.
+ * @param {Function} props.setPreviewVisibility - The hook for setting editor visibility.
+ * @returns {React.Component} The component.
+ */
+const ArticleEditorForm = ({
+  article,
+  cancelFunc,
+  compileFillerImages,
+  confirmFunc,
+  confirmText,
+  handlers,
+  heading,
+  isPublish,
+  operation,
+  previewVisible,
+  removeFillerImage,
+  setImagesChanged,
+  setPreviewVisibility
+}) => {
   const [isLoaded, setLoaded] = useState(false);
   const [authors, setAuthors] = useState([]);
   const [isDateFieldVisible, setVisibility] = useState(false);
@@ -76,165 +130,195 @@ const ArticleForm = ({
         setLoaded(true);
       }
     });
-  }, [isLoaded]);
+  }, [isLoaded, previewVisible]);
 
   useEffect(() => {
     setVisibility(article.status === ARTICLE_STATUS.PUBLISHED);
   });
 
   return (
-    <Shader>
-      <Spacer className={css['article-form']}>
-        <div>
-          <Heading>{heading}</Heading>
+    <>
+      <div
+        className={
+          css[previewVisible ? 'article-editor-form' : 'article-form']
+        }>
+        <Group>
+          <Col lg>
+            <Heading>{heading}</Heading>
+          </Col>
+          <Col lg className={css['preview-toggle-container']}>
+            <button className={css['preview-toggle-button']} onClick={() => setPreviewVisibility(!previewVisible)}>
+              {previewVisible ? 'Hide Preview' : 'Show Preview'}
+            </button>
+          </Col>
+        </Group>
 
-          <Group>
-            <Col md={7}>
-              <Label>Title:</Label>
-              <TextInput
-                name={'title'}
-                value={article.title}
-                onChange={handleText}
-                placeholder={'Enter the title.'}
-              />
-            </Col>
-            <Col md={5}>
-              <Label>Category:</Label>
-              <Select
-                name={'category'}
-                value={article.category}
-                placeholder={'Select a category.'}
-                items={categories}
-                onChange={handleText}
-              />
-            </Col>
-          </Group>
-          <Group>
-            <Col>
-              <LabelInfo>Content:</LabelInfo>
-              <LongTextArea
-                name={'content'}
-                value={article.content}
-                onChange={handleText}
-                placeholder={'Write your thoughts. Express yourself.'}
-              />
-            </Col>
-          </Group>
-          <Group>
-            <Col>
-              <Label>Excerpt:</Label>
-              <ShortTextArea
-                name={'excerpt'}
-                value={article.excerpt}
-                onChange={handleText}
-                placeholder={"Enter this article's excerpt."}
-              />
-            </Col>
-          </Group>
-          <Group>
-            <Col md={6}>
-              <Label>Author:</Label>
-              <Select
-                name={'authorId'}
-                value={article.authorId}
-                placeholder={'Select the author.'}
-                items={authors}
-                onChange={handleText}
-              />
-            </Col>
-            <Col md={{ span: 4, offset: 2 }}>
-              <Label>Status:</Label>
-              <Select
-                name={'status'}
-                value={article.status}
-                placeholder={'Select a status.'}
-                items={Object.keys(ARTICLE_STATUS).map((key) => key)}
-                onChange={handleText}
-              />
-            </Col>
-          </Group>
-          <DatePublished
-            isDateFieldVisible={isDateFieldVisible}
-            datePublished={article.datePublished}
-            handleDate={handleDate}
-          />
-          <Group>
-            <Col>
-              <Label>Tags:</Label>
-              <ShortTextArea
-                name={'tags'}
-                value={article.tags}
-                onChange={handleText}
-                placeholder={
-                  'Add a comma-separated list of tags (e.g. woke, society, black women)'
-                }
-              />
-            </Col>
-          </Group>
-          <Group>
-            <Col sm={6}>
-              <FileSelector
-                image={article.coverImage}
-                operation={operation}
-                onChange={(img) => {
-                  handleFile(img, 'coverImage');
-                  setImagesChanged(true);
-                }}
-                placeholder={"Choose this article's cover image..."}
-                removeImage={() => {
-                  removeFile();
-                  setImagesChanged(true);
-                }}
-                aspectRatio={ASPECT_RATIO.WIDE}
-                selectorLook={SELECTOR_LOOK.PLACEHOLDER}
-              />
-            </Col>
-          </Group>
-          <Group>
-            <Col>
-              <Label>Additional Images:</Label>
-              <FillerImagesGroup
-                article={article}
-                operation={operation}
-                compileFillerImages={compileFillerImages}
-                removeImage={removeFillerImage}
-              />
-            </Col>
-          </Group>
-        </div>
-
-        <div>
-          <Group>
-            <Col>
-              <SubmitButton
-                onClick={
-                  isPublish
-                    ? () => setPublishModalVisibility(true)
-                    : confirmFunc
-                }
-                className={'mr-2'}>
-                {confirmText}
-              </SubmitButton>
-              <CancelButton onClick={cancelFunc}>Cancel</CancelButton>
-            </Col>
-          </Group>
-        </div>
-
-        <ConfirmModal
-          visible={isPublishModalVisible}
-          message={
-            "By publishing this article, you'll be notifying all subscribers of this new release. Please confirm that you want to publish."
-          }
-          confirmFunc={confirmFunc}
-          confirmText={'Confirm'}
-          close={() => setPublishModalVisibility(false)}
+        <Group>
+          <Col md={7}>
+            <Label>Title:</Label>
+            <TextInput
+              name={'title'}
+              value={article.title}
+              onChange={handleText}
+              placeholder={'Enter the title.'}
+            />
+          </Col>
+          <Col md={5}>
+            <Label>Category:</Label>
+            <Select
+              name={'category'}
+              value={article.category}
+              placeholder={'Select a category.'}
+              items={categories}
+              onChange={handleText}
+            />
+          </Col>
+        </Group>
+        <Group>
+          <Col>
+            <LabelInfo>Content:</LabelInfo>
+            <LongTextArea
+              name={'content'}
+              value={article.content}
+              placeholder={'Write your thoughts. Express yourself.'}
+              onChange={handleText}
+            />
+          </Col>
+        </Group>
+        <Group>
+          <Col>
+            <Label>Excerpt:</Label>
+            <ShortTextArea
+              name={'excerpt'}
+              value={article.excerpt}
+              onChange={handleText}
+              placeholder={"Enter this article's excerpt."}
+            />
+          </Col>
+        </Group>
+        <Group>
+          <Col md={6}>
+            <Label>Author:</Label>
+            <Select
+              name={'authorId'}
+              value={article.authorId}
+              placeholder={'Select the author.'}
+              items={authors}
+              onChange={handleText}
+            />
+          </Col>
+          <Col md={{ span: 4, offset: 2 }}>
+            <Label>Status:</Label>
+            <Select
+              name={'status'}
+              value={article.status}
+              placeholder={'Select a status.'}
+              items={Object.keys(ARTICLE_STATUS).map((key) => key)}
+              onChange={handleText}
+            />
+          </Col>
+        </Group>
+        <DatePublishedField
+          isDateFieldVisible={isDateFieldVisible}
+          datePublished={article.datePublished}
+          handleDate={handleDate}
         />
-      </Spacer>
-    </Shader>
+        <Group>
+          <Col>
+            <Label>Tags:</Label>
+            <ShortTextArea
+              name={'tags'}
+              value={article.tags}
+              onChange={handleText}
+              placeholder={
+                'Add a comma-separated list of tags (e.g. woke, society, black women)'
+              }
+            />
+          </Col>
+        </Group>
+        <Group>
+          <Col sm={6}>
+            <FileSelector
+              image={article.coverImage}
+              operation={operation}
+              onChange={(img) => {
+                handleFile(img, 'coverImage');
+                setImagesChanged(true);
+              }}
+              placeholder={"Choose this article's cover image..."}
+              removeImage={() => {
+                removeFile();
+                setImagesChanged(true);
+              }}
+              aspectRatio={ASPECT_RATIO.WIDE}
+              selectorLook={SELECTOR_LOOK.PLACEHOLDER}
+            />
+          </Col>
+        </Group>
+        <Group>
+          <Col>
+            <Label>Additional Images:</Label>
+            <FillerImagesGroup
+              article={article}
+              operation={operation}
+              compileFillerImages={compileFillerImages}
+              removeImage={removeFillerImage}
+            />
+          </Col>
+        </Group>
+        <Group>
+          <Col>
+            <SubmitButton
+              onClick={
+                isPublish ? () => setPublishModalVisibility(true) : confirmFunc
+              }
+              className={'mr-2'}>
+              {confirmText}
+            </SubmitButton>
+            <CancelButton onClick={cancelFunc}>Cancel</CancelButton>
+          </Col>
+        </Group>
+      </div>
+
+      <ConfirmModal
+        visible={isPublishModalVisible}
+        message={
+          "By publishing this article, you'll be notifying all subscribers of this new release. Please confirm that you want to publish."
+        }
+        confirmFunc={confirmFunc}
+        confirmText={'Confirm'}
+        close={() => setPublishModalVisibility(false)}
+      />
+    </>
   );
 };
 
-const DatePublished = ({ isDateFieldVisible, datePublished, handleDate }) => {
+/**
+ * The editor preview of the article content.
+ * @param {object} props - The component props.
+ * @param {string} props.text - The article content to be previewed.
+ * @param {boolean} props.visible - Indicates whether the editor preview should be visible.
+ * @returns {React.Component} The component.
+ */
+const ArticleEditorPreview = ({ text, visible }) => {
+  if (!visible) return null;
+
+  return (
+    <div className={css['article-editor-preview']}>
+      <Paragraph>{text}</Paragraph>
+    </div>
+  );
+};
+
+/**
+ * The date published field.
+ * @param {object} props - The component props.
+ * @param {string} props.datePublished - The date the article was published.
+ * @param {string} props.handleDate - The hook for handling date changes.
+ * @param {boolean} props.isDateFieldVisible - Indicates whether this field should be visible.
+ * @returns {React.Component} The component.
+ */
+const DatePublishedField = ({ isDateFieldVisible, datePublished, handleDate }) => {
   if (!isDateFieldVisible) return null;
 
   return (
@@ -251,6 +335,15 @@ const DatePublished = ({ isDateFieldVisible, datePublished, handleDate }) => {
   );
 };
 
+/**
+ * The article filler images.
+ * @param {object} props - The component props.
+ * @param {string} props.article - The article object.
+ * @param {string} props.compileFillerImages - compileFillerImages - The hook for compiling article filler images.
+ * @param {boolean} props.operation - Either a CREATE or an UPDATE.
+ * @param {boolean} props.removeImage - The function for removing a filler image from selection.
+ * @returns {React.Component} The component.
+ */
 const FillerImagesGroup = ({
   article,
   operation,
