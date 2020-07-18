c548a4358daf1ad8b51239dc33f05a302c3b792d
Zavid <42748748+zzavidd@users.noreply.github.com>
2020-07-19 00:55:52 +0100

    Show preview text on article editor (#67)
    * Add new textareas local to this project
    
    * Show preview of paragraphs while editing
    
    * Keep form buttons at bottom
    
    * Allow image substitutions in editing

---
+++ src/components/form/v2/textarea.js
@@ -0,0 +1,48 @@
+import classNames from 'classnames';
+import React, { useState, useEffect } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
+
+import css from 'styles/components/Form.module.scss';
+
+const TextArea = (props) => {
+  const { minRows, className, name, placeholder, value = '', onChange } = props;
+
+  return (
+    <TextareaAutosize
+      name={name}
+      placeholder={placeholder}
+      className={classNames(css['textarea-v2'], className)}
+      rows={minRows}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export const ShortTextArea = (props) => {
+  const { onChange } = props;
+  return <TextArea {...props} minRows={1} onChange={onChange} />;
+};
+
+export const LongTextArea = (props) => {
+  const { onChange, value } = props;
+  const [wordCount, setWordCount] = useState(0);
+
+  useEffect(() => {
+    setWordCount(value.length);
+  }, [value]);
+
+  return (
+    <>
+      <TextArea
+        {...props}
+        minRows={3}
+        onChange={(e) => {
+          onChange(e);
+          setWordCount(e.target.value.length);
+        }}
+      />
+      <label className={css['word-count']}>{wordCount}</label>
+    </>
+  );
+};
