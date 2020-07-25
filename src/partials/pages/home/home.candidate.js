4fbe6accade76720c24fc8fd1255ca55594cb78b
Zavid <zavidegbue@gmail.com>
2020-07-25 11:20:30 +0100

    Use country flags on homepage

--- src/partials/pages/home/home.candidate.js
+++ src/partials/pages/home/home.candidate.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import LazyLoader from 'react-visibility-sensor';
-import { zDate, zText } from 'zavid-modules';
+import React, { useState, useEffect } from 'react';
+import { zDate } from 'zavid-modules';
 
+import { CountryFlags } from 'components/emoji.js';
+import { LazyLoader } from 'components/loader.js';
 import {
   Title,
   Subtitle,
@@ -11,104 +11,91 @@ import {
   VanillaLink
 } from 'components/text.js';
 import { Fader } from 'components/transitioner.js';
-import { countriesToString } from 'constants/countries.js';
 import request from 'constants/request.js';
 import { cloudinary } from 'constants/settings.js';
 import css from 'styles/pages/Home.module.scss';
 
-class RandomCandidate extends Component {
-  constructor() {
-    super();
-    this.state = {
-      candidate: {},
-      inView: false,
-      detectViewChange: true
-    };
-  }
+const RandomCandidate = () => {
+  const [candidate, setCandidate] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
+  const [isInView, setInView] = useState(false);
 
-  componentDidMount() {
-    this.getRandomCandidate();
-  }
+  useEffect(() => {
+    getRandomCandidate();
+  }, [isLoaded]);
 
-  toggleVisibility = (inView) => {
-    this.setState({ inView, detectViewChange: inView === false });
-  };
-
-  getRandomCandidate = () => {
+  const getRandomCandidate = () => {
     request({
       url: '/api/v1/candidates/random',
       method: 'GET',
       headers: { Authorization: process.env.AUTH_KEY },
       onSuccess: (candidate) => {
-        if (candidate) {
-          candidate.loaded = true;
-          this.setState({ candidate });
+        candidate.firstname = candidate.name.split(' ')[0];
+        candidate.age = zDate.calculateAge(candidate.birthday);
+        try {
+          candidate.description =
+            candidate.description.trim() || 'No description.';
+        } catch (ex) {
+          candidate.description = 'No description.';
         }
+        setCandidate(candidate);
+        setLoaded(true);
       }
     });
   };
 
-  render() {
-    const { candidate, inView, detectViewChange } = this.state;
-    const { countries } = this.props;
+  const link = `/blackexcellence/${candidate.id}`;
 
-    if (candidate.name && candidate.loaded) {
-      candidate.firstname = candidate.name.split(' ')[0];
-      candidate.age = zDate.calculateAge(candidate.birthday);
-      candidate.description =
-        candidate.description.trim().length > 0
-          ? candidate.description
-          : 'No description.';
-      candidate.demonyms = countriesToString(
-        JSON.parse(candidate.ethnicity),
-        countries
-      );
-    }
+  return (
+    <div className={css['home-random-candidate']}>
+      <LazyLoader setInView={setInView}>
+        <Fader determinant={isInView} duration={750}>
+          <Title className={css['heading']}>Check out our candidate:</Title>
+          <div>
+            <CandidateImage candidate={candidate} link={link} />
+            <CandidatePreview candidate={candidate} link={link} />
+          </div>
+        </Fader>
+      </LazyLoader>
+    </div>
+  );
+};
 
-    const link = `/blackexcellence/${candidate.id}`;
+const CandidateImage = ({ candidate, link }) => {
+  if (!candidate.image) return null;
+  return (
+    <VanillaLink href={link}>
+      <img
+        src={`${cloudinary.url}/${candidate.image}`}
+        alt={candidate.name}
+        className={css['image']}
+      />
+    </VanillaLink>
+  );
+};
 
-    return (
-      <div className={css.randomCandidate}>
-        <LazyLoader
-          onChange={this.toggleVisibility}
-          partialVisibility={true}
-          active={detectViewChange}>
-          <Fader determinant={inView} duration={750}>
-            <Title className={css.heading}>Check out our candidate:</Title>
-            <div>
-              {candidate.image ? (
-                <VanillaLink href={link}>
-                  <img
-                    src={`${cloudinary.url}/${candidate.image}`}
-                    alt={candidate.name}
-                    className={css.image}
-                  />
-                </VanillaLink>
-              ) : null}
-              <div className={css.details}>
-                <Title className={css.title}>{candidate.name}</Title>
-                <Subtitle className={css.subtitle}>
-                  {candidate.age} • {candidate.occupation} •{' '}
-                  {candidate.demonyms}
-                </Subtitle>
-                <Divider />
-                <Paragraph
-                  truncate={45}
-                  morelink={link}
-                  moretext={`Discover more on ${candidate.firstname}`}
-                  className={css.paragraph}>
-                  {candidate.description}
-                </Paragraph>
-              </div>
-            </div>
-          </Fader>
-        </LazyLoader>
-      </div>
-    );
-  }
-}
-const mapStateToProps = (state) => ({
-  countries: state.countries
-});
+const CandidatePreview = ({ candidate, link }) => {
+  return (
+    <div className={css['details']}>
+      <Title className={css['title']}>{candidate.name}</Title>
+      <Subtitle className={css['subtitle']}>
+        {candidate.age} • {candidate.occupation}
+        <CountryFlags
+          ethnicities={candidate.ethnicity}
+          size={25}
+          className={css['home-country-flags']}
+        />
+      </Subtitle>
+      <Divider />
+      <Paragraph
+        truncate={45}
+        morelink={link}
+        moretext={`Discover more on ${candidate.firstname}`}
+        className={css['paragraph']}>
+        {candidate.description}
+      </Paragraph>
+    </div>
+  );
+};
 
-export default connect(mapStateToProps)(RandomCandidate);
+export default RandomCandidate;
