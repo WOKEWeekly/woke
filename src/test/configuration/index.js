2bad9b57822f64fad7a95b197627b8fe4b8774f6
Omotola <omotolashogunle@gmail.com>
2020-07-28 18:52:49 +0100

    Changed exports.module to individual exports in test/configuration/index.js - exports now seperated to assert, HEADERS and request

--- src/test/configuration/index.js
+++ src/test/configuration/index.js
@@ -3,6 +3,17 @@ const axios = require('axios');
 const { startTestServer, isStageTesting, port } = require('../../server.js');
 axios.defaults.baseURL = `http://localhost:${port}`;
 
+/**
+  * Abstract function for HTTP requests.
+  * @param {string} request.url - The url to make the request to.
+  * @param {string} [request.method] - The method of the request. Defaults to GET.
+  * @param {object} [request.body] - The payload for the request.
+  * @param {object} [request.headers] - The headers to accompany the request.
+  * @param {Function} [request.onSuccess] - Function triggered on successful request.
+  * @param {Function} [request.onError] - Function triggered on successful request.
+  * @param {Function} [request.done] - The callback to finish the test.
+*/
+
 // Check if user is dev before running test.
 if (process.env.MYSQL_USER !== 'dev') {
   console.error('Trying to run test with user that is not dev');
@@ -22,58 +33,48 @@ after(function () {
   setTimeout(() => process.exit(0), 2000);
 });
 
-module.exports = {
-  /** For the assertion library */
-  assert: require('chai').assert,
+exports.request = ({ 
+  url,
+  method = 'GET',
+  body,
+  headers = {},
+  onSuccess,
+  onError,
+  done
 
-  /**
-   * Abstract function for HTTP requests.
-   * @param {string} request.url - The url to make the request to.
-   * @param {string} [request.method] - The method of the request. Defaults to GET.
-   * @param {object} [request.body] - The payload for the request.
-   * @param {object} [request.headers] - The headers to accompany the request.
-   * @param {Function} [request.onSuccess] - Function triggered on successful request.
-   * @param {Function} [request.onError] - Function triggered on successful request.
-   * @param {Function} [request.done] - The callback to finish the test.
-   */
-  request: ({
+}) => {
+  headers['Content-Type'] = 'application/json';
+
+  axios({
     url,
-    method = 'GET',
-    body,
-    headers = {},
-    onSuccess,
-    onError,
-    done
-  }) => {
-    headers['Content-Type'] = 'application/json';
-
-    axios({
-      url,
-      method,
-      data: body,
-      headers
+    method,
+    data: body,
+    headers
+  })
+    .then((response) => {
+      if (onSuccess) onSuccess(response);
+      done();
     })
-      .then((response) => {
-        if (onSuccess) onSuccess(response);
+    .catch((error) => {
+      if (typeof onError === 'function') {
+        onError(error.response);
         done();
-      })
-      .catch((error) => {
-        if (typeof onError === 'function') {
-          onError(error.response);
-          done();
-        } else {
-          done(error);
-        }
-      });
-  },
+      } else {
+        done(error);
+      }
+    });
+};
 
-  /** The header options used for valid requests */
-  HEADERS: {
-    TOKEN: (user) => ({
-      Authorization: `Bearer ${user.token}`
-    }),
-    KEY: {
-      Authorization: process.env.AUTH_KEY
-    }
+/** The header options used for valid requests */
+exports.HEADERS = {
+  TOKEN: (user) => ({
+    Authorization: `Bearer ${user.token}`
+  }),
+  KEY: {
+    Authorization: process.env.AUTH_KEY
   }
 };
+
+ /** For the assertion library */
+exports.assert = require('chai').assert;
+
